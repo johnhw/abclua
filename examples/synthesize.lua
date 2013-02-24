ff2f1e7b5f3e435ab545857b7c6ea93545e37899
johnhw@gmail.com <johnhw@gmail.com@3c15d5a3-047b-a0a4-bf95-714db52f5fbf>
2013-02-24 20:12:29 +0000

    Synthesize example works correctly

--- examples/synthesize.lua
+++ examples/synthesize.lua
@@ -19,7 +19,9 @@ end
 function write_word(file, value)
     -- write little endian word to a binary file
     file:write(string.char(math.floor(value%256)))
+   
     file:write(string.char(math.floor((value/256)%256)))
+   
 end
 
 function write_wav_header(file, samples, sr)
@@ -34,11 +36,75 @@ function write_wav_header(file, samples, sr)
     write_word(file, 1) -- PCM
     write_word(file, 1) -- one channel
     write_dword(file, sr) -- sample rate
-    write_dword(file, sr) -- byte rate
+    write_dword(file, sr*2) -- byte rate
     write_word(file, 1) -- block align
-    write_word(file, 8)
+    write_word(file, 16)
     file:write('data')
     write_dword(file, samples) -- data block length
+end
+
+function synthesize_note(wav, sr, duration, pitch, beat_time, tempo)
+    local f, fr, amp, vamp, t, velocity, attack
+    local decay = 500
+    
+        if pitch then
+            f = 2*midi_to_frequency(pitch)/sr
+             -- reset envelope
+            amp, vamp = 0.01, 0.01
+        else
+            f = 0 -- rest
+            amp, vamp = 0, 0 
+        end
+        
+        -- dodgy swing
+        if math.floor(beat_time)%2 == 0 then
+            duration = duration * 1.05
+        else
+            duration = duration * 0.95
+        end
+        
+        -- emphasise first beat, and strong beats in the meter
+        if beat_time==0 then 
+            velocity = 1.0
+        else    
+            -- emphasize "on beat" notes
+            if math.abs(beat_time-math.floor(beat_time))<0.01 then
+                velocity = 0.7
+                -- duration = duration * 0.95
+            else
+                velocity = 0.5
+                -- duration = duration * 0.9
+            end
+        end
+        
+        local vsquared = velocity * velocity
+        -- simulate hitting the note slightly flat to begin 
+        local fr = 0.94*f
+        attack = true
+            
+        -- there's no point in being efficient here: Lua is not
+        -- the right choice anyway!
+        for i=1,duration do  
+            -- compute simple fm oscillator with vibrato
+            vib = math.sin(i/1100)*vamp 
+            value =  math.sin(i*fr*2*math.pi*(1+vib/1400)) * amp * (1+vib/3) 
+            value = (value/2 * 32767) 
+            
+            if value<0 then value = 65536+value end
+            
+            -- update envelope
+            fr = 0.998*fr + 0.002*f
+            if vamp<1.8 then vamp = vamp * 1.0004 end
+         
+            if duration-i<decay then
+                amp = amp * 0.97
+            else
+                if attack and amp<velocity then amp=amp*(1+0.008*vsquared) else attack = false end
+                if not attack and amp>0.6 then amp=amp*0.9997 end
+            end
+            wav:write(string.char(math.floor(value%256)))
+            wav:write(string.char(math.floor((value/256)%256)))
+        end     
 end
 
 function synthesize(fname)
@@ -50,72 +116,27 @@ function synthesize(fname)
     local wav = io.open(wav_name, 'wb')
     local duration = duration_stream(notes)
     local sr = 22100
+    local tempo = songs[1].voices['default'].context.tempo.tempo_rate
     local sample_time = 1e6/sr
     local samples = math.floor(sr*duration/1e6)
-    write_wav_header(wav, samples, sr)
     local t = 0
     local sample_duration
-    local level, velocity, amp, vamp, mamp, decay, w, vib
-    decay = 3000
+    local velocity, amp, vamp, decay, vib
+    
+    write_wav_header(wav, samples, sr)
+    decay = 300
     for i,v in ipairs(notes) do
         if v.event=='note' or v.event=='rest' then
-            if t<v.t then
-                --advance pointer, writing out 0 values 
-                wav:write(string.rep(string.char(127), ((v.t-t)/sample_time)))
-                t = v.t
-            end                       
+            -- if t<v.t then
+                -- -- advance pointer, writing out 0 values 
+                -- for i=1,(v.t-t)/sample_time do
+                    -- write_word(wav, 0)
+                -- end
+                -- t = v.t
+            -- end                       
             -- write the note out
             sample_duration = v.duration / sample_time
-            
-            -- whistle tune: up one octave and slightly flat high notes
-            if v.note.play_pitch then
-                f = 2*midi_to_frequency(v.note.play_pitch*0.999)/sr
-            else
-                f = 0 -- rest
-            end
-            
-            -- reset envelope
-            amp = 0.01
-            vamp = 0.01
-            mamp = 0.01
-            
-            -- emphasise first beat, and strong beats in the meter
-            if v.note.play_bar_time==0 then 
-                velocity = 1.0 
-                sample_duration = sample_duration * 1.05
-            else    
-                t = meter*v.note.play_bar_time
-                if math.abs(t-math.floor(t))<0.01 then
-                    velocity = 0.85
-                    sample_duration = sample_duration * 0.95
-                else
-                    velocity = 0.7
-                    sample_duration = sample_duration * 0.9
-                end
-            end
-            
-            local vsquared = velocity * velocity
-            -- simulate hitting the note slightly flat to begin 
-            local fr = 0.9*f
-            -- there's no point in being efficient here: Lua is not
-            -- the right choice anyway!
-            for i=1,sample_duration do  
-                -- compute simple fm oscillator with vibrato
-                vib = math.sin(i/1500)*vamp
-                value =  math.sin(i*fr*2*math.pi+vib+math.sin(i*fr*4*math.pi)*0.5*vsquared*(1-mamp))*amp
-                fr = 0.997*fr + 0.003*f
-                -- update envelope
-                value = (value/2 * 127) + 128
-                if vamp<1.8 then vamp = vamp * 1.0008 end
-                if mamp<1 then mamp = mamp * 1.006 end
-             
-                if sample_duration-i<decay then
-                    amp = amp * 0.999
-                else
-                    if amp<velocity then amp=amp*(1+0.01*vsquared) end                
-                end
-                wav:write(string.char(value))                
-            end     
+            synthesize_note(wav, sr, sample_duration, v.note.play_pitch, v.note.play_bar_time*meter, tempo)
             t = v.t+sample_duration*sample_time
         end
     end
